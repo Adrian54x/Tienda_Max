ed34fd7d1b0f8969022b810c58245742219f5de9
adria <adria@REX54X>
2026-03-20 08:12:54 -0600

    Agregar archivos de proyecto.

---
+++ Tienda_Max/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+Dictionary<int, Productos> productos = new Dictionary<int, Productos>();
+int codigo;
+class Productos
+{
+    public string nombre;
+    public double precio;
+    public void MostrarProductos()
+    {
+        Console.WriteLine($"");
+    }
+}
